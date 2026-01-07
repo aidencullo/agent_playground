777ef920c89d3b9486dec29052c06507b722236f
aidencullo <aidencullo@gmail.com>
2026-01-07 16:39:32 -0500

    Remove all files except Dockerfile and .gitignore

--- src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-
-function App() {
-  return <h1>react app in nginx!</h1>
-}
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
